0eb81606029a25777463c06564666af2eaa2dea7
Gustavo Ehrhardt <gus.ehr@gmail.com>
2016-07-02 12:35:23 -0300

    firebaseImport para web

---
+++ firebaseImport.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/3.1.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBeEyhMtxVS5oYiY7SscM9_j_1dL9ruRFw",
+    authDomain: "mobcast-371fe.firebaseapp.com",
+    databaseURL: "https://mobcast-371fe.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+</script>
+    
